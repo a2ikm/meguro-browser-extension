3406449897fdf842e041fc1e56f5e6241764f810
Masato Ikeda <masato.ikeda@gmail.com>
2021-08-29 20:43:26 +0900

    Add bookmark from chrome extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+/* global chrome */
 
 function App() {
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let tab = tabs[0]
+      document.getElementById("form-title").setAttribute("value", tab.title)
+      document.getElementById("form-url").setAttribute("value", tab.url)
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="https://desolate-harbor-23973.herokuapp.com/_/add" method="POST">
+        <input type="text" name="title" id="form-title" />
+        <input type="url" name="url" id="form-url" />
+        <input type="text" name="tags" id="form-tags" />
+        <textarea name="comment" id="form-comment"></textarea>
+        <input type="submit" value="Add" />
+      </form>
     </div>
   );
 }
