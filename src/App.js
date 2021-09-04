e204a505da3c54356d61c4ca9ae544f62b861dae
Masato Ikeda <masato.ikeda@gmail.com>
2021-09-05 00:46:47 +0900

    Close on posted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 /* global chrome */
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [url, setURL] = useState("")
+  const [tags, setTags] = useState("")
+  const [comment, setComment] = useState("")
+
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       let tab = tabs[0]
-      document.getElementById("form-title").setAttribute("value", tab.title)
-      document.getElementById("form-url").setAttribute("value", tab.url)
+      setTitle(tab.title)
+      setURL(tab.url)
     })
-  })
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch(`${process.env.REACT_APP_MEGURO_URL}/_/add`, {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        url: url,
+        tags: tags,
+        comment: comment,
+      })
+    })
+      .then(res => window.close())
+      .catch(e => console.log(e))
+  }
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleURLChange = (event) => {
+    setURL(event.target.value)
+  }
+
+  const handleTagsChange = (event) => {
+    setTags(event.target.value)
+  }
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value)
+  }
 
   return (
     <div className="App">
-      <form action={`${process.env.REACT_APP_MEGURO_URL}/_/add`} method="POST">
-        <input type="text" name="title" id="form-title" />
-        <input type="url" name="url" id="form-url" />
-        <input type="text" name="tags" id="form-tags" />
-        <textarea name="comment" id="form-comment"></textarea>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" value={title} onChange={handleTitleChange} />
+        <input type="url" name="url" value={url} onChange={handleURLChange} />
+        <input type="text" name="tags" value={tags} onChange={handleTagsChange} />
+        <textarea name="comment" value={comment} onChange={handleCommentChange}></textarea>
         <input type="submit" value="Add" />
       </form>
     </div>
